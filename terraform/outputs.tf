a2a61d73b84f1632bfe074d5eb1d5cfae48b0955
maxengna <phanupong.w2019@gmail.com>
2026-05-21 17:31:16 +0700

    change variable to be dynamic

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,12 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "alb_role_arn" {
+  value = aws_iam_role.alb.arn
+}
